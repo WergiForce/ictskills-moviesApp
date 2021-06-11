0017d288851d60c7b93353de79b77ce395b55bd4
Ivan_de_Wergifosse <ivandewergifosse@hotmail.com>
2021-06-11 17:01:36 +0100

    Added Upcoming movies page

--- src/index.js
+++ src/index.js
@@ -6,12 +6,14 @@ import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
 import FavoriteMoviesPage from "./pages/favoriteMoviesPage";
 import MovieReviewPage from "./pages/movieReviewPage";
+import UpcomingMoviesPage from "./pages/upcomingMoviesPage"
 
 const App = () => {
   return (
     <BrowserRouter>
           <SiteHeader />      {/* New Header  */}
       <Switch>
+        <Route exact path="/movies/upcoming" component={UpcomingMoviesPage} />
         <Route path="/reviews/:id" component={MovieReviewPage} />
         <Route exact path="/movies/favorites" component={FavoriteMoviesPage} />
         <Route path="/movies/:id" component={MoviePage} />
